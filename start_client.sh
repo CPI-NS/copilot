295490ca1c6e4a3bc7700cad60f533b4650b27f9
Shawn Zimmermann <zimmermannshawn@gmail.com>
2024-11-05 11:51:12 -0700

    start scripts

---
+++ start_client.sh
@@ -0,0 +1,1 @@
+./lib/ClientDriverGo -maddr=node1 -mport=7087 -twoLeaders=true -id=0
--- start_copilot.sh
+++ start_copilot.sh
@@ -6,13 +6,13 @@
 
 
 # use .all to get a list of nodes
-all_nodes_addrs = ($(echo "@.all_sut_nodes.host@" | tr ':' ' ')) 
+all_nodes_addrs=($(echo "@.all.host@" | tr ':' ' '))
 
 
 # -1 for the master
-N=$(expr ${#all_node_addrs[@]} - 1)
-if [ .me.host == .all[0]; then
-  ./bin/master -N $N -twoLeaders=true
+N=$(expr @.all | length@ - 1)
+if [ "@.me.host@" == "@.all[0].host@" ]; then
+  ./lib/master -N $N -twoLeaders=true
 else
-  ./bin/server -maddr=@.me.maddr@ -mport=@.me.mport@ -addr=@.me.host@ -port=@.me.port@ -copilot=true -exec=true -dreply=true
+  ./lib/server -maddr=@.me.maddr@ -mport=@.me.mport@ -addr=@.me.host@ -port=@.me.port@ -copilot=true -exec=true -dreply=true
 fi
