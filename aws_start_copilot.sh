9d35abcbd5cc4eaa65f973f02bbca0cf34c6de60
shawn zimmermann <zimmermannshawn@gmail.com>
2024-11-19 19:37:26 +0000

    aws_start

---
+++ aws_start_copilot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copilot start script
+#
+##################
+
+
+# use .all to get a list of nodes
+all_nodes_addrs=($(echo "@.all.host@" | tr ':' ' '))
+
+
+# -1 for the master
+N=$(expr @.all | length@ - 1)
+if [ "@.me.host@" == "@.all[0].host@" ]; then
+  ./lib/master -N $N -twoLeaders=true
+else
+  ./lib/server -maddr=@.me.maddr@ -mport=@.me.mport@ -addr=@.me.paddr@ -port=@.me.port@ -copilot=true -exec=true -dreply=true
+fi
